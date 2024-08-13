35dc49bfe6799ccc9bf50211edb288ddbc03ffc0
Tanya Rath <emailtanya8@gmail.com>
2024-08-12 20:46:51 -0700

    basic login elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.module.css';
+import styles from './App.module.css';
 
 function App() {
-  
+  return (
+    <div className={styles.App}>
+      <div className={styles.welcomeInfo}>
+        <h1>Ja<span>mmmm</span>ing</h1>
+        <button><i class='bx bxl-spotify'></i>LOGIN WITH SPOTIFY</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
