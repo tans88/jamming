44a5a8d7f641a3d54fb26c384ee31877d31f6ae8
Tanya Rath <emailtanya8@gmail.com>
2024-08-18 10:07:31 -0700

    starting spotify auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.module.css';
 import styles from './App.module.css';
+import {Spotify} from '/Applications/Desktop/personal_projects/jamming/src/util/Spotify.js';
 
 function App() {
+
   return (
     <div className={styles.App}>
+      {/* Sign in with Spotify */}
       <div className={styles.welcomeInfo}>
         <h1>Ja<span>mmmm</span>ing</h1>
-        <button><i class='bx bxl-spotify'></i>LOGIN WITH SPOTIFY</button>
+        <button onClick={Spotify}><i class='bx bxl-spotify'></i>LOGIN WITH SPOTIFY</button>
       </div>
     </div>
   );
---
+++ src/util/Spotify.js
@@ -0,0 +1,10 @@
+const CLIENT_ID = '05aeb2be6cf54fed86ee4b4aa793e116';
+const REDIRECT_URI = 'http://localhost:3000/callback';
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const SCOPES = "playlist-modify-public playlist-modify-private";
+
+const Spotify = () => {
+    console.log("hi");
+};
+
+export default Spotify;
