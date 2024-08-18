340f7914e18da9c4635d60e3e20f4aa5fb85de65
Tanya Rath <emailtanya8@gmail.com>
2024-08-18 16:42:41 -0700

    finished auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.module.css';
-import styles from './App.module.css';
-import {Spotify} from '/Applications/Desktop/personal_projects/jamming/src/util/Spotify.js';
+import styles from './App.module.css'
+
+import { Spotify } from '../../util/Spotify';
+import wavingHand from './waving-hand@2x.gif';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('');
 
-  return (
-    <div className={styles.App}>
-      {/* Sign in with Spotify */}
-      <div className={styles.welcomeInfo}>
-        <h1>Ja<span>mmmm</span>ing</h1>
-        <button onClick={Spotify}><i class='bx bxl-spotify'></i>LOGIN WITH SPOTIFY</button>
+  useEffect(() => {
+    const token = Spotify.checkAuth();
+    if (token){
+      setIsLoggedIn(true);
+      // get user's 
+      Spotify.getUserName()
+        .then((fetchName) => {
+          setUserName(fetchName);
+        })
+        .catch((error) => {
+          console.error('Error fetching user name:', error);
+        });
+    }
+  }, []);
+
+  const handleLoginButtonClick = () => {
+    Spotify.getAuth();
+  }
+
+  if (!isLoggedIn){
+    return (
+      <div className={styles.App}>
+        {/* Sign in with Spotify */}
+        <div className={styles.welcomeInfo}>
+          <h1>Ja<span>mmmm</span>ing</h1>
+          <button onClick={handleLoginButtonClick}><i class='bx bxl-spotify'></i>LOGIN WITH SPOTIFY</button>
+        </div>
       </div>
-    </div>
-  );
+    )
+  } else{
+    return (
+      <div className={styles.App}>
+        <div className={styles.home}>
+          <h1>Hey there, {userName} <img src={wavingHand} alt="waving hand"/></h1>
+          <p>Ready to make your next playlist?</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,10 +1,48 @@
 const CLIENT_ID = '05aeb2be6cf54fed86ee4b4aa793e116';
 const REDIRECT_URI = 'http://localhost:3000/callback';
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const SCOPES = "playlist-modify-public playlist-modify-private";
+let accessToken;
+let userName;
 
-const Spotify = () => {
-    console.log("hi");
-};
+export const Spotify = {
+    // Redirect user to Spotify Auth page when loggin button is clicked
+    getAuth () {
+        const tokenURL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+        window.location = tokenURL;
+    },
 
-export default Spotify;
+    // Checks URL on page load - if there's a token to stract, it runs.
+    checkAuth() {
+        const authenticated = window.location.href.match(/access_token=([^&]*)/);
+        if (authenticated) {
+            accessToken = authenticated[1];
+            return accessToken;
+        } else {
+            return false;
+        }
+    },
+
+    getUserName() {
+        if(!accessToken) {
+            return Promise.reject(new Error('Access token is missing'));
+        }
+        const nameEndpoint = 'https://api.spotify.com/v1/me';
+        return fetch(nameEndpoint, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            }
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch user data');
+             }
+        })
+        .then((data) => {
+            userName = data.display_name;
+            const userId = data.id;
+            return userName;         
+        });
+    },
+}
